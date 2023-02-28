c37209b7a5b9f8e5551fa575b282f51801620411
butterriolu <derpvin12@gmail.com>
2023-02-28 01:58:52 -0500

    Add logo

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import githubLogo from "../../assets/images/github-logo.png";
+import logo from "../../assets/images/logo.png";
 import "./navbar.css";
+
 export default function Navbar() {
   return (
     <nav>
       <div className="navbar-content">
+        <div>
+          <img src={logo} className="github-logo" alt="github-logo" />
+        </div>
         <div>Home</div>
         <div>Search</div>
         <div>Saved</div>
