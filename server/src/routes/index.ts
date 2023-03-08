faf51e9010ec4b0dfe4dc0ed88eb7c44ffb4acc7
manav809 <manavpatel6978@gmail.com>
2023-03-08 14:45:55 -0500

    Connected to MongoDB

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,19 @@
 import { Express } from "express";
 import listEndpoints from "express-list-endpoints";
 import ExampleRouter from "./example";
+import mongoose from "mongoose";
+
+import * as dotenv from  "dotenv";
+dotenv.config();
 
 async function generateRoutes(app: Express): Promise<void> {
   app.use("/example", ExampleRouter);
-
+  mongoose
+    .connect(process.env.CONNECTION_URL!)
+    .then(() => {
+      console.log("Successfully Connected To MongoDB");
+    })
+    .catch(console.log)
   console.table(listEndpoints(app));
 }
 
