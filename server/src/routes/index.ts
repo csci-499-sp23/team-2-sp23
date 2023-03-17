c4551b37a4a7fe70249c79bc26babe364fc36859
butterriolu <derpvin12@gmail.com>
2023-03-17 19:50:18 -0400

    Setup middleware folder

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,12 +4,10 @@ import listEndpoints from "express-list-endpoints";
 import mongoose from "mongoose";
 
 import * as dotenv from "dotenv";
-import { warCrimes } from "../controllers/war-crimes";
 dotenv.config();
 
 async function generateRoutes(app: Express): Promise<void> {
   app.use("/example", ExampleRouter);
-  app.get("/scrape", warCrimes);
 
   mongoose
     .connect(process.env.CONNECTION_URL!)
