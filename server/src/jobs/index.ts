8c1d02df4ff6a57b9bc757b52e444a28d0b5bb98
butterriolu <derpvin12@gmail.com>
2023-03-22 01:41:12 -0400

    Automate war-crimes on a 30 minute interval

---
+++ server/src/jobs/index.ts
@@ -0,0 +1,14 @@
+import cron from "node-cron";
+import { scrapeCoordinates } from "./tasks/scrape-coordinates";
+
+function scheduleJobs() {
+  const everyThirtyMinutes = "*/30 * * * *";
+
+  scrapeCoordinates();
+  cron.schedule(everyThirtyMinutes, async () => {
+    // pick a random coordinate and scrape it
+    await scrapeCoordinates();
+  });
+}
+
+export default scheduleJobs;
