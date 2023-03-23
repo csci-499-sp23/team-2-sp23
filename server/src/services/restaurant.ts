2d5f91e817735dd235335363f52a437cdac112ee
butterriolu <derpvin12@gmail.com>
2023-03-23 00:51:12 -0400

    Add restaurant-service/find-near-within-budget

--- server/src/services/restaurant.ts
+++ server/src/services/restaurant.ts
@@ -6,6 +6,7 @@ import {
   RestaurantModel,
 } from "../models/Restaurant";
 import { MenuModel } from "../models/Menu";
+import { FoodDocument } from "../models/Food";
 
 async function create(
   restaurant: RestaurantAttributes
@@ -68,10 +69,97 @@ async function findNear(
   ]);
 }
 
+export type RestaurantResult = {
+  restaurant: RestaurantDocument;
+  foods: FoodDocument;
+};
+
+async function findNearWithinBudget(
+  coordinates: Coordinates,
+  searchRadius: Meters,
+  budget: number
+): Promise<RestaurantResult[]> {
+  const filterNearbyRestaurants = {
+    $geoNear: {
+      near: {
+        type: "Point",
+        coordinates: coordinates,
+      },
+      distanceField: "distance",
+      maxDistance: searchRadius,
+      spherical: true,
+    },
+  };
+  const setRestaurantInObject = {
+    $project: {
+      _id: false,
+      restaurant: "$$ROOT",
+    },
+  };
+  const leftJoinMenus = {
+    $lookup: {
+      from: "menus",
+      localField: "restaurant.menu_id",
+      foreignField: "_id",
+      as: "menu",
+    },
+  };
+  const unwindMenus = {
+    $unwind: "$menu",
+  };
+  const leftJoinFoods = {
+    $lookup: {
+      from: "foods",
+      localField: "menu.foods",
+      foreignField: "_id",
+      as: "foods",
+    },
+  };
+  const removeMenuField = {
+    $unset: ["menu"],
+  };
+  const filterFoodsInBudget = {
+    $addFields: {
+      foods: {
+        $filter: {
+          input: "$foods",
+          cond: { $lte: ["$$food.price", budget] },
+          as: "food",
+        },
+      },
+    },
+  };
+  const addFoodCountField = {
+    $addFields: {
+      food_count: { $size: "$foods" },
+    },
+  };
+  const pickRestaurantsInBudget = {
+    $match: {
+      food_count: { $ne: 0 },
+    },
+  };
+
+  const fatassQuery: any[] = [
+    filterNearbyRestaurants,
+    setRestaurantInObject,
+    leftJoinMenus,
+    unwindMenus,
+    leftJoinFoods,
+    removeMenuField,
+    filterFoodsInBudget,
+    addFoodCountField,
+    pickRestaurantsInBudget,
+  ];
+
+  return RestaurantModel.aggregate(fatassQuery);
+}
+
 export default {
   create,
   upsert,
   exists,
   updateMenu,
   findNear,
+  findNearWithinBudget,
 };
