eb79b860f60284eb8c171e264821455f90ca834c
butterriolu <derpvin12@gmail.com>
2023-03-23 03:31:40 -0400

    Update nearby restaurant test

--- server/tests/services/restaurants/tests/retrieve-nearby.ts
+++ server/tests/services/restaurants/tests/retrieve-nearby.ts
@@ -2,14 +2,23 @@ import RestaurantService from "../../../../src/services/restaurant";
 import { RestaurantModel } from "../../../../src/models/Restaurant";
 import { testRestaurants } from "../../constants/restaurants";
 import { expect } from "@jest/globals";
+import { generateMenuId } from "../../menus/tests/utils";
 
 export async function testRetrieveNearbyRestaurants() {
-  await RestaurantModel.insertMany(
+  const createdRestaurantIds = await RestaurantModel.insertMany(
     testRestaurants.map((restaurant) => ({
       ...restaurant,
       created_at: new Date(),
     }))
-  );
+  ).then((res) => res.map((restaurant) => restaurant._id));
+
+  for (const restaurantId of createdRestaurantIds) {
+    const menuId = await generateMenuId(restaurantId);
+    await RestaurantModel.findByIdAndUpdate(
+      { _id: restaurantId },
+      { menu_id: menuId }
+    );
+  }
 
   const nearbyRestaurants = await RestaurantService.findNear(
     [-74.00565, 40.74207],
@@ -17,10 +26,10 @@ export async function testRetrieveNearbyRestaurants() {
   );
 
   const nearbyRestaurantNames = nearbyRestaurants?.map(
-    (restaurant) => restaurant.name
+    (result) => result.restaurant.name
   );
 
-  expect(nearbyRestaurants!.length).toBe(3);
+  expect(nearbyRestaurants.length).toBe(3);
   expect(nearbyRestaurantNames).toEqual([
     "Very Fresh Noodles",
     "Omusubi Gonbei",
