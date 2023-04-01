0f5019d72ac5d2384bea35367b57ab48e3914115
butterriolu <derpvin12@gmail.com>
2023-03-31 20:56:45 -0400

    Add restaurant header (image + name + categories)

--- client/src/pages/Restaurant/RestaurantView/Header.js
+++ client/src/pages/Restaurant/RestaurantView/Header.js
@@ -1,12 +1,84 @@
+import { useEffect, useState } from "react";
+import { FastAverageColor } from "fast-average-color";
+
 const classes = {
   container: {
+    height: "144px",
+    // ignore parent padding
+    marginInline: "-1rem",
+    marginTop: "-1rem",
+    position: "relative",
+    overflow: "hidden",
+  },
+  banner: {
+    position: "absolute",
     width: "100%",
-    height: "400px",
-    backgroundColor: "gray",
+    objectFit: "cover",
+    height: "100%",
+  },
+  bottomBlur: {
+    width: "100%",
+    height: "100%",
+    background:
+      "linear-gradient(0deg, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.025))",
+  },
+  content: {
+    position: "absolute",
+    bottom: 0,
+    marginTop: "auto",
+    display: "flex",
+    alignItems: "flex-end",
+    gap: "1rem",
+    paddingInline: "1rem",
+    paddingBottom: "1rem",
+    color: "white",
+  },
+  profile: {
+    width: "84px",
+    height: "84px",
+    objectFit: "cover",
+    borderRadius: "1rem",
+    border: "2px solid white",
+  },
+  restaurantName: {
+    fontSize: "1.25rem",
+  },
+  categories: {
+    fontSize: "0.75rem",
+    color: "#c9c9c9",
   },
 };
-function Header() {
-  return <div style={classes.container}>Header</div>;
+
+function Header({ imageUrl, name, foodCategories }) {
+  const [headerColor, setHeaderColor] = useState(null);
+  const colorRender = new FastAverageColor();
+
+  // calculates the average color of an image
+  // sets headerColor when completed, otherwise uses white
+  async function getAverageColor(imageUrl) {
+    await colorRender
+      .getColorAsync(imageUrl)
+      .then((color) => setHeaderColor(color.hex))
+      .catch(() => setHeaderColor("white"));
+  }
+
+  useEffect(() => {
+    getAverageColor(imageUrl);
+    // eslint-disable-next-line
+  }, [imageUrl]);
+
+  return (
+    <div style={{ ...classes.container, backgroundColor: headerColor }}>
+      <div style={classes.bottomBlur} />
+      <div style={classes.content}>
+        <img src={imageUrl} style={classes.profile} alt={name} />
+        <div style={{ marginBottom: "0.25rem" }}>
+          <div style={classes.restaurantName}>{name}</div>
+          <div style={classes.categories}>{foodCategories.join(" • ")}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- client/src/pages/Restaurant/RestaurantView/index.js
+++ client/src/pages/Restaurant/RestaurantView/index.js
@@ -1,21 +1,22 @@
-import React from "react";
 import Header from "./Header";
 import RedirectButton from "../../../components/RedirectButton";
 import YelpLogo from "../../../assets/images/yelp-logo.png";
 import GoogleMapsLogo from "../../../assets/images/google-maps-logo.png";
 
 export default function RestaurantView({ restaurant, foods }) {
-  const [long, lat] = restaurant.location.coordinates;
+  const [longitude, latitude] = restaurant.location.coordinates;
 
   return (
     <div>
-      <Header />
-      {restaurant.name}
-      {foods.length}
+      <Header
+        imageUrl={restaurant.image_url}
+        name={restaurant.name}
+        foodCategories={restaurant.food_categories}
+      />
       <RedirectButton image={YelpLogo} url={restaurant.yelp_url} />
       <RedirectButton
         image={GoogleMapsLogo}
-        url={`https://www.google.com/maps/search/${lat},${long}`}
+        url={`https://www.google.com/maps/search/${latitude},${longitude}`}
       />
     </div>
   );
