68c8c81d278b93526034860ff6f4113f3e3a3b8f
Remos96 <omer122@protonmail.com>
2023-04-07 20:41:52 -0400

    Add Food Cards on Restaurant Page

--- client/src/pages/Restaurant/FoodList/FoodCard.js
+++ client/src/pages/Restaurant/FoodList/FoodCard.js
@@ -1,5 +1,51 @@
-function FoodCard() {
-  return <div>FoodCard</div>;
+function FoodCard({ name, price, description, imageUrl }) {
+  const classes = {
+    cardContainer: {
+      boxSizing: "border-box",
+      width: "100%",
+      border: "2px solid #5a5a5a",
+      padding: "0.125rem",
+      display: "flex",
+      gap: "0.25rem",
+      backgroundColor: "hsl(35,0%,97%)",
+    },
+    foodPrice: {
+      fontSize: "1rem",
+      marginLeft: "auto",
+      padding: "1rem",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+    },
+    foodDescription: {
+      fontSize: "0.75rem",
+      color: "#3A3A3A",
+      overflow: "auto",
+      whiteSpace: "normal",
+      maxHeight: "72px",
+      borderLeft: "2px solid",
+      borderColor: "hsl(30,80%,40%)",
+      paddingLeft: "0.25rem",
+    },
+    image: {
+      minWidth: "52px",
+      width: "52px",
+      height: "52px",
+      objectFit: "cover",
+      borderRadius: "2px",
+    },
+  };
+  
+  return (
+    <div style={classes.cardContainer}>
+      <img src={imageUrl} style={classes.image} alt="Food Item" />
+      <div>
+        <div>{name}</div>
+        <div style={classes.foodDescription}>{description}</div>
+      </div>
+      <div style={classes.foodPrice}>${price.toFixed(2)}</div>
+    </div>
+  );
 }
 
 export default FoodCard;
--- client/src/pages/Restaurant/FoodList/index.js
+++ client/src/pages/Restaurant/FoodList/index.js
@@ -1,5 +1,27 @@
-function FoodList() {
-  return <div>FoodList</div>;
+import FoodCard from "./FoodCard";
+
+const classes = {
+  foodList: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "0.75rem",
+  },
+};
+
+function FoodList({ foods }) {
+  return (
+    <div style={classes.foodList}>
+      {foods.map((food, i) => (
+        <FoodCard
+          key={i}
+          name={food.name}
+          price={food.price}
+          description={food.description}
+          imageUrl={food.image_url}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default FoodList;
