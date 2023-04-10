1dccbd440bb5479f00f74b01227606d2d61df6d1
manav809 <manavpatel6978@gmail.com>
2023-04-10 17:56:42 -0400

    Add Address and Phone Number for Restaurant

--- client/src/pages/Restaurant/RestaurantView/index.js
+++ client/src/pages/Restaurant/RestaurantView/index.js
@@ -3,6 +3,7 @@ import RedirectButton from "../../../components/RedirectButton";
 import YelpLogo from "../../../assets/images/yelp-logo.png";
 import GoogleMapsLogo from "../../../assets/images/google-maps-logo.png";
 import FoodList from "../FoodList";
+import RestaurantInfo from "./RestaurantInfo";
 
 export default function RestaurantView({ restaurant, foods }) {
   const [longitude, latitude] = restaurant.location.coordinates;
@@ -10,6 +11,7 @@ export default function RestaurantView({ restaurant, foods }) {
   return (
     <div>
       <Header restaurant={restaurant} />
+      <RestaurantInfo restaurant={restaurant} />
       <RedirectButton image={YelpLogo} url={restaurant.yelp_url} />
       <RedirectButton
         image={GoogleMapsLogo}
