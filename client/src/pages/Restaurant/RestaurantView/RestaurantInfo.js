5d386dbc5cbcbc352b88295fca64a36f519867cb
butterriolu <derpvin12@gmail.com>
2023-04-15 17:54:45 -0400

    Re-align restaurant info

--- client/src/pages/Restaurant/RestaurantView/RestaurantInfo.js
+++ client/src/pages/Restaurant/RestaurantView/RestaurantInfo.js
@@ -1,26 +1,46 @@
+import RedirectButton from "../../../components/RedirectButton";
+import YelpLogo from "../../../assets/images/yelp-logo.png";
+import GoogleMapsLogo from "../../../assets/images/google-maps-logo.png";
 import { formatPhoneNumber } from "../../../utils/formatPhoneNumber";
 
 const classes = {
+  container: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
   addressContainer: {
-    fontSize: "0.9rem",
     color: "hsl(30, 80%, 40%)",
   },
   phoneNumberContainer: {
-    fontSize: "0.9rem",
+    color: "black",
   },
 };
 
 export default function RestaurantInfo({ restaurant }) {
-  const phoneNumber = formatPhoneNumber(restaurant.phone);
-  const { address1, city, state, zip_code } = restaurant.address;
+  const { address, location, phone } = restaurant;
+  const { address1, city, state, zip_code } = address;
+  const [longitude, latitude] = location.coordinates;
+
+  const phoneNumber = formatPhoneNumber(phone);
 
   return (
-    <div>
-      <div style={classes.addressContainer}>
-        {address1} {city}, {state} {zip_code}
+    <div style={classes.container}>
+      <div>
+        <div style={classes.addressContainer}>
+          {address1} {city}, {state} {zip_code}
+        </div>
+        <div>
+          <a style={classes.phoneNumberContainer} href={`tel: ${phoneNumber}`}>
+            {phoneNumber}
+          </a>
+        </div>
       </div>
-      <div style={classes.phoneNumberContainer}>
-        <a href={`tel: ${phoneNumber}`}>{phoneNumber}</a>
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <RedirectButton image={YelpLogo} url={restaurant.yelp_url} />
+        <RedirectButton
+          image={GoogleMapsLogo}
+          url={`https://www.google.com/maps/search/${latitude},${longitude}`}
+        />
       </div>
     </div>
   );
--- client/src/pages/Restaurant/RestaurantView/index.js
+++ client/src/pages/Restaurant/RestaurantView/index.js
@@ -1,22 +1,13 @@
 import Header from "./Header";
-import RedirectButton from "../../../components/RedirectButton";
-import YelpLogo from "../../../assets/images/yelp-logo.png";
-import GoogleMapsLogo from "../../../assets/images/google-maps-logo.png";
+
 import FoodList from "../FoodList";
 import RestaurantInfo from "./RestaurantInfo";
 
 export default function RestaurantView({ restaurant, foods }) {
-  const [longitude, latitude] = restaurant.location.coordinates;
-
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}>
       <Header restaurant={restaurant} />
       <RestaurantInfo restaurant={restaurant} />
-      <RedirectButton image={YelpLogo} url={restaurant.yelp_url} />
-      <RedirectButton
-        image={GoogleMapsLogo}
-        url={`https://www.google.com/maps/search/${latitude},${longitude}`}
-      />
       <FoodList foods={foods} />
     </div>
   );
