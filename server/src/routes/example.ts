c45f0d6fc8eb05d1d93da29650108c7c07709dbc
JoshuaRamc <joshuaramcharan818@gmail.com>
2023-04-20 22:46:20 -0400

    Delete example files

--- server/src/routes/example.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import ExampleController from "../controllers/example";
-
-const router = express.Router();
-
-router.get("/get", ExampleController.getCash, ExampleController.presentCash);
-router.post("/add", ExampleController.addCash, ExampleController.presentCash);
-
-export default router;
