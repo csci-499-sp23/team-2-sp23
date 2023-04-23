c7f8f2c661dade51e42740c1dff5ba2db167c713
manav809 <manavpatel6978@gmail.com>
2023-04-22 20:18:57 -0400

    Add Default Image to Restaurant Preview

--- client/src/pages/Search/MapView/RestaurantPreview.js
+++ client/src/pages/Search/MapView/RestaurantPreview.js
@@ -4,6 +4,7 @@ import { IconButton, Button } from "@mui/material";
 import CancelIcon from "@mui/icons-material/Cancel";
 import { Link } from "react-router-dom";
 import FastfoodIcon from "@mui/icons-material/Fastfood";
+import defaultRestaurantImg from "../../../assets/images/default-restaurant-img.png";
 
 const classes = {
   previewContainer: {
@@ -96,8 +97,8 @@ function RestaurantPreview({
       <div style={{ display: "flex" }}>
         <img
           style={classes.imageContainer}
-          src={restaurant.image_url}
-          alt={restaurant.image_url}
+          src={restaurant.image_url || defaultRestaurantImg}
+          alt={"restaurant"}
         />
         <div style={classes.detailsContainer}>
           <Link style={classes.nameContainer} to={restaurantPage}>
@@ -133,11 +134,9 @@ function RestaurantPreview({
             openModal();
           }}
         >
-          <span style={{ fontWeight: 400 }}>
-            {foods.length}
-          </span>
+          <span style={{ fontWeight: 400 }}>{foods.length}</span>
           <FastfoodIcon
-            style={{ color: "hsl(30,90%,50%)", fontSize: "1rem"}}
+            style={{ color: "hsl(30,90%,50%)", fontSize: "1rem" }}
           />
         </Button>
       </div>
