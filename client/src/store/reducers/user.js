7aaf9c697ad55423633180a8a5fa17f7bcef3ffa
butterriolu <derpvin12@gmail.com>
2023-05-06 16:50:20 -0400

    Add user slice

---
+++ client/src/store/reducers/user.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  _id: null,
+  auth0_id: null,
+  username: null,
+  profile_picture_url: null,
+  saved_restaurants: [],
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      const user = action.payload;
+      state._id = user._id;
+      state.auth0_id = user.auth0_id;
+      state.username = user.username;
+      state.profile_picture_url = user.profile_picture_url;
+      state.saved_restaurants = user.saved_restaurants;
+    },
+    logout: (state, action) => {
+      state.auth0_id = null;
+      state.username = null;
+      state.profile_picture_url = null;
+      state.saved_restaurants = [];
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+export default userSlice.reducer;
