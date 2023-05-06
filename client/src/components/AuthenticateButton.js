eb710d25ede43162f4b1e1c6421c97bae0037b01
butterriolu <derpvin12@gmail.com>
2023-05-06 18:18:11 -0400

    Store auth0user to user slice

--- client/src/components/AuthenticateButton.js
+++ client/src/components/AuthenticateButton.js
@@ -1,13 +1,34 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "@mui/material";
+import { useEffect } from "react";
+import { login, logout } from "../store/reducers/user";
+import { useDispatch } from "react-redux";
 
 export default function AuthenticateButton() {
+  const dispatch = useDispatch();
   const {
+    user: Auth0User,
     loginWithRedirect: Auth0Login,
     logout: Auth0Logout,
     isAuthenticated,
   } = useAuth0();
-  
+
+  useEffect(() => {
+    if (!isAuthenticated) dispatch(logout());
+    else {
+      const formattedUser = {
+        _id: null,
+        auth0_id: Auth0User.sub,
+        username: Auth0User.name,
+        profile_picture_url: Auth0User.picture,
+        saved_restaurants: [],
+      };
+
+      dispatch(login(formattedUser));
+    }
+    // eslint-disable-next-line
+  }, [Auth0User]);
+
   return isAuthenticated ? (
     <Button onClick={() => Auth0Logout()}>Logout</Button>
   ) : (
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import progressReducer from "./reducers/progress";
 import foodCategoriesReducer from "./reducers/foodCategories";
+import userReducer from "./reducers/user";
 
 export const store = configureStore({
   reducer: {
     progress: progressReducer,
     foodCategories: foodCategoriesReducer,
+    user: userReducer,
   },
 });
