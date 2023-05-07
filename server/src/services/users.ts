196ac60d6aec2a86b0ef1909c305b2fa12788e45
butterriolu <derpvin12@gmail.com>
2023-05-06 22:28:17 -0400

    Add create user route

--- server/src/services/users.ts
+++ server/src/services/users.ts
@@ -1,21 +1,25 @@
 import { UserAttributes, UserModel } from "../models/User";
 
-async function getUserByAuth0Id(
-  auth0Id: string
-): Promise<UserAttributes | null> {
-  return await UserModel.findOne({ auth0_id: auth0Id });
+async function findOne(query: any): Promise<UserAttributes | null> {
+  return await UserModel.findOne(query);
 }
 
-async function create(user: UserAttributes): Promise<UserAttributes> {
+async function create(auth0Id: string): Promise<UserAttributes> {
   return UserModel.create({
-    ...user,
+    auth0_id: auth0Id,
     created_at: new Date(),
   });
 }
 
+async function exists(query: any): Promise<boolean> {
+  const userExists = !!(await UserModel.count(query));
+  return userExists;
+}
+
 const UserService = {
-  getUserByAuth0Id,
+  findOne,
   create,
+  exists,
 };
 
 export default UserService;
