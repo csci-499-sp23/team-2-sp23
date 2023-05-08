a1cced9ada8659933e060c4e69ea5bc0b173df8f
manav809 <manavpatel6978@gmail.com>
2023-05-07 21:39:10 -0400

    Add Save Restaurant

--- server/src/services/users.ts
+++ server/src/services/users.ts
@@ -1,4 +1,6 @@
 import { UserAttributes, UserModel } from "../models/User";
+import { ObjectId } from "mongoose";
+import { RestaurantModel } from "../models/Restaurant";
 
 async function findOne(query: any): Promise<UserAttributes | null> {
   return await UserModel.findOne(query);
@@ -16,10 +18,22 @@ async function exists(query: any): Promise<boolean> {
   return userExists;
 }
 
+async function saveRestaurant(
+  userId: ObjectId,
+  restaurantId: ObjectId
+): Promise<UserAttributes | null> {
+  const user = await UserModel.findById(userId);
+  const restaurant = await RestaurantModel.findById(restaurantId);
+  user?.saved_restaurants.push(restaurantId);
+  restaurant?.saved_by?.push(userId);
+  return user;
+}
+
 const UserService = {
   findOne,
   create,
   exists,
+  saveRestaurant,
 };
 
 export default UserService;
