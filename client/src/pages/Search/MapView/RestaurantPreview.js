ab58a40dec834f720416866ed703e9e5b195c7ad
manav809 <manavpatel6978@gmail.com>
2023-05-15 00:15:55 -0400

    Fix

--- client/src/pages/Search/MapView/RestaurantPreview.js
+++ client/src/pages/Search/MapView/RestaurantPreview.js
@@ -22,7 +22,7 @@ const classes = {
   },
   imageContainer: {
     height: "100%",
-    width: "100px",
+    width: "120px",
     borderRadius: "2px",
     objectFit: "cover",
     objectPosition: "center center",
@@ -34,7 +34,7 @@ const classes = {
     fontSize: "0.8rem",
   },
   nameContainer: {
-    fontSize: "1rem",
+    fontSize: "1.25rem",
     color: "black",
     width: "fit-content",
   },
@@ -116,11 +116,6 @@ function RestaurantPreview({
           <Link style={classes.nameContainer} to={restaurantPage}>
             {restaurant.name}
           </Link>
-
-          <a href={`http://maps.google.com/?q=${restaurantAddress}`}>
-            <div style={{ color: "hsl(30,80%,40%)" }}>{restaurantAddress}</div>
-          </a>
-
           <div style={classes.reviewContainer}>
             <ReviewStars
               rating={restaurant.rating}
